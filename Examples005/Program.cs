a5fca27e0152ede3ceb365252d4e6118a8d47b4d
Ivan <Ermak_Ermakowi4@mail.ru>
2022-12-08 02:54:16 +0300

    Задание 10

---
+++ Examples005/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine ("Введите трёхзначное число");
+
+string numberStr = Console.ReadLine ();
+int number = Convert.ToInt32 (numberStr);
+int result = 0;
+
+result = (number % 100) /10;
+Console.WriteLine (result);
