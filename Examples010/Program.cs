00beb5003f391609097538a9df6bad05929041fb
Ivan <Ermak_Ermakowi4@mail.ru>
2022-12-12 01:36:56 +0300

    Задание 23

---
+++ Examples010/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine ("Введите чилсо N");
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= count; i++ ) {
+    double x = Math.Pow (i, 3);
+    Console.WriteLine (x);
+}
