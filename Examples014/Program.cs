e4fd04b492b12aea245feb9fde3625e0f817ed55
Ivan <Ermak_Ermakowi4@mail.ru>
2022-12-20 01:55:57 +0300

    Задание 34

--- Examples014/Program.cs
+++ Examples014/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-int[] Array = new int [100];
+int[] array = new int [100];
 Random rand = new Random();
 int count = 0;
 
-for (int i = 0; i < Array.Length; i++) {
-    Array[i] = rand.Next(100,1000);
-   if (Array[i] % 2 == 0 || Array[i] % 4 == 0 || Array[i] % 6 == 0 || Array[i] % 2 == 8  ) {
+for (int i = 0; i < array.Length; i++) {
+    array[i] = rand.Next(100,1000);
+   if (array[i] % 2 == 0 || array[i] % 4 == 0 || array[i] % 6 == 0 || array[i] % 2 == 8  ) {
         count++;
         }
-    Console.Write(Array[i] + ", ");   
+    Console.Write(array[i] + ", ");   
 }   
 Console.WriteLine();
 Console.WriteLine(count);
