e895b08d5aee619d26c4468a68de9d3806286b8c
Nord <Ermak_Ermakowi@mail.ru>
2022-12-29 17:46:28 +0300

    update Examples009

--- Examples009/Program.cs
+++ Examples009/Program.cs
@@ -9,10 +9,10 @@ int x2 = int.Parse(Console.ReadLine());
 int y2 = int.Parse(Console.ReadLine());
 int z2 = int.Parse(Console.ReadLine());
 
-double distanc (int x1, int y1, int z1, int x2, int y2, int z2){
+double distance (int x1, int y1, int z1, int x2, int y2, int z2){
 
     double result = 0;
     result = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
     return result;
 }
-Console.WriteLine ($"Расстояние между точками = {distanc (x1,y1,z1,x2,y2,z2)}");
+Console.WriteLine ($"Расстояние между точками = {distance (x1,y1,z1,x2,y2,z2)}");
