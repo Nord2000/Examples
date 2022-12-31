1700d5d4d7f97041081aece64d083e402df58031
Nord <Ermak_Ermakowi@mail.ru>
2022-12-31 14:59:45 +0300

    Задание 68

---
+++ Examples029/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+uint m = 2;
+uint n = 3;
+
+uint Fun(uint n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else if ((n != 0) && (m == 0))
+        return Fun(n - 1, 1);
+    else
+        return Fun(n - 1, Fun(n, m - 1));
+}
+
+Console.WriteLine(Fun(m,n));
