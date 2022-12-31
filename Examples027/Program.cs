d30e0ecdbc18f6ad070cd89017604bc35db3b5ac
Nord <Ermak_Ermakowi@mail.ru>
2022-12-31 13:53:14 +0300

    Задание 64

---
+++ Examples027/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string naturalOfNumberN(int N)
+{
+    if(N < 1) return String.Empty;
+    return $"{N} {naturalOfNumberN(N - 1)}";
+}
+
+Console.WriteLine(naturalOfNumberN(N));
